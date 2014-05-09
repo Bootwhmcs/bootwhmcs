0a706f11288bde50d2b8c15db7f80a09b286a36b
Freddy Lundekvam <freddy@wearelogik.com>
2014-05-09 16:37:28 +0200

    Ticket rating fixed. Fixed #20

--- bootwhmcs/static/js/app.js
+++ bootwhmcs/static/js/app.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function(){
-  
-  
+
+
   /** Determine password strength **/
   /*********************************/
-  
+
   jQuery("#password").keyup(function () {
     var pw = jQuery("#password").val();
     var pwlength=(pw.length);
@@ -20,26 +20,26 @@ jQuery(document).ready(function(){
     var pwstrength=((pwlength*10)-20)+(numeric*10)+(numsymbols*15)+(upper*10);
     if(pwstrength<0){pwstrength=0}
     if(pwstrength>100){pwstrength=100}
-    
+
     jQuery("#pwstrengthbox").removeClass("weak moderate strong");
     jQuery("#pwstrengthbox").html("Strong");
     jQuery("#pwstrengthbox").addClass("strong");
-    
+
     if (pwstrength<75) {
       jQuery("#pwstrengthbox").html("Moderate");
       jQuery("#pwstrengthbox").addClass("moderate");
     }
-    
+
     if (pwstrength<30) {
       jQuery("#pwstrengthbox").html("Weak");
       jQuery("#pwstrengthbox").addClass("weak");
     }
   });
-  
-  
+
+
   /** Enable facebook recommend **/
   /*******************************/
-  
+
   (function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) {return;}
@@ -47,34 +47,34 @@ jQuery(document).ready(function(){
     js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
-  
-  
+
+
   (function() {
     var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
     po.src = 'https://apis.google.com/js/plusone.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
   })();
-  
+
   jQuery('.multiTLD').click(function() {
     jQuery('#tlds').slideToggle();
     return false;
   });
-  
+
   // Facebook 100% Width
   // ===============================
   var foundFBComs = false;
   $('.fb-comments-unloaded').each(function(){
     var $fbCom = $(this),
     contWidth = $fbCom.parent().width();
-  
+
     $fbCom.attr('data-width', contWidth).removeClass('fb-comments-unloaded').addClass('fb-comments');
     foundFBComs = true;
   });
-  
+
   if(foundFBComs) {
     FB.XFBML.parse();
   }
-  
+
   // Open Centered Popup
   // ===============================
   function popupWindow(addr,popname,w,h,features) {
@@ -90,9 +90,9 @@ jQuery(document).ready(function(){
     win = window.open(addr,popname,settings);
     win.window.focus();
   }
-  
-  
-  
+
+
+
   jQuery("#subaccount").click(function () {
     if (jQuery("#subaccount:checked").val()!=null) {
       jQuery("#subaccountfields").slideDown();
@@ -100,12 +100,35 @@ jQuery(document).ready(function(){
       jQuery("#subaccountfields").slideUp();
     }
   });
-  
+
   $("#country").select2();
-  
-  $('.extraTicketAttachment').click(function() { 
+
+  $('.extraTicketAttachment').click(function() {
     jQuery("#fileuploads").append('<input type="file" name="attachments[]" style="width:70%;" /><br />');
     return false;
   });
-  
+
 });
+
+// Support Tickets
+// ===============================
+
+function extraTicketAttachment() {
+    jQuery("#fileuploads").append('<input type="file" name="attachments[]" style="width:70%;" /><br />');
+}
+
+function rating_hover(id) {
+    var selrating=id.split('_');
+    for(var i=1; i<=5; i++){
+        if(i<=selrating[1]) document.getElementById(selrating[0]+'_'+i).style.background="url(images/rating_pos.png)";
+        if(i>selrating[1]) document.getElementById(selrating[0]+'_'+i).style.background="url(images/rating_neg.png)";
+    }
+}
+function rating_leave(id){
+    for(var i=1; i<=5; i++){
+        document.getElementById(id+'_'+i).style.background="url(images/rating_neg.png)";
+    }
+}
+function rating_select(tid,c,id){
+    window.location='viewticket.php?tid='+tid+'&c='+c+'&rating='+id;
+}
